e628caa171185d83619435f23a0459eecb444b2e
AbdulSalamAbro <macherki.nessrine@yahoo.com>
2024-08-03 16:02:29 +0100

    01-setup the project and the required files

---
+++ asset/js/config.js
@@ -0,0 +1,12 @@
+// config.js
+
+const _configSettings = {
+    "selection": 394, 
+    "booster": 15, 
+    "amount": 5, 
+    "hallOfFame": 5, 
+    "totalCost": 100, 
+    "totalPriority": 30
+};
+
+export default _configSettings;
